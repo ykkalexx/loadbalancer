95373a235beee26c4af862bbbf16663049a8a0c4
ykkalexx <alex.barbatescu@icloud.com>
2025-02-16 11:53:51 +0000

    Initial project setup

---
+++ servers-for-testing/server_01.py
@@ -0,0 +1,9 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "Hello from Server 1"
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=5001)
---
+++ src/go.mod
@@ -0,0 +1,3 @@
+module github.com/ykkalexx/load-balancer
+
+go 1.23.0
