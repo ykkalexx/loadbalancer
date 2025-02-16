2aeff87b3b686db8ea6b8b1b60210c81e1d2e151
ykkalexx <alex.barbatescu@icloud.com>
2025-02-16 12:00:34 +0000

    [wip] created initial load balancer

---
+++ src/internal/load_balancer.go
@@ -0,0 +1,39 @@
+package loadbalancer
+
+import "sync"
+
+// represents a backend server
+type Server struct {
+	URL   string
+	Alive bool
+	mux   sync.RWMutex
+	FailCount int
+}
+
+// configs for load balancer
+type loadBalancer struct {
+	servers []*Server
+	mux sync.RWMutex
+	// roundrobin counter
+	current int
+}
+
+// init new load balancer
+func NewLoadBalancer() *loadBalancer {
+	return &loadBalancer{
+		servers: make([]*Server, 0),
+		current: 0,
+	}
+}
+
+// used to add a new server to the load balancer
+func (lb *loadBalancer) AddServer(url string) {
+	lb.mux.Lock()
+	defer lb.mux.Unlock()
+
+	server := &Server{
+		URL: url,
+		Alive: true,
+	}
+	lb.servers = append(lb.servers, server)
+}
